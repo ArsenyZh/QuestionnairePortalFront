1d0f26fc08519db1026f53ebab0ac57eb77d50b7
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-05-05 17:54:48 +0300

    add logic to show name of user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,25 @@
+import { useNavigate } from "react-router-dom"
+import { user } from "../services/UserService"
+import React, { useEffect, useState } from "react"
+
 export default function Header() {
+
+    const navigator = useNavigate();
+
+    const [userName, setUserName] = useState([])
+
+    useEffect(() => {
+        user().then((response) => {
+            setUserName(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    }, [])
+
+    function Profile() {
+        navigator('/user')
+    }
+
     return (
         <div>
             <header>
@@ -8,19 +29,19 @@ export default function Header() {
                         <div className="collapse navbar-collapse justify-content-md-end " id="navbarDropdownMenuLink">
                             <ul className="navbar-nav">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Fields</a>
+                                    <a className="nav-link" href="/fields">Fields</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Responses</a>
+                                    <a className="nav-link" href="/responses">Responses</a>
                                 </li>
                                 
                                 <li className="nav-item dropdown">
-                                    <button className="nav-link dropdown-toggle" type="button" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                                        User
+                                    <button className="nav-link dropdown-toggle" onClick={Profile} type="button" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                                        {userName.firstName + userName.lastName}
                                     </button>
                                     <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                        <a className="dropdown-item" href="#">Edit Profile</a>
-                                        <a className="dropdown-item" href="#">Change Password</a>
+                                        <a className="dropdown-item" href="/edit-profile">Edit Profile</a>
+                                        <a className="dropdown-item" href="/change-password">Change Password</a>
                                         <a className="dropdown-item" href="#">Log Out</a>
                                     </div>
                                 </li>
