b3a06a1d9672d3655832935267d298b3ae6d854c
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-05-13 21:16:26 +0300

    add methods to edit info and change password

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = "http://localhost:8080/user/1"
+const id = 1
 
-export const user = () => axios.get(REST_API_BASE_URL )
+const REST_API_BASE_URL = "http://localhost:8080/user/"
+
+export const user = () => axios.get(REST_API_BASE_URL + id)
+
+export const editUserInfo = (profile) => axios.post(REST_API_BASE_URL + "edit_info/" + id, profile)
+
+export const changeUserPassword = (passForm) => axios.post(REST_API_BASE_URL + "change_pas/" + id, passForm)
 
 // import axios from "axios";
 
