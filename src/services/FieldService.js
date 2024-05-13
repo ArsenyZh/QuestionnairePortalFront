c47b8575145e259496c44ff1b22eb0719f7449bf
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-05-13 21:16:59 +0300

    add methods to update and delete fields

--- src/services/FieldService.js
+++ src/services/FieldService.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
-const REST_API_GET_URL = "http://localhost:8080/fields"
-const REST_API_POST_URL = "http://localhost:8080/fields/create"
+const REST_API_URL = "http://localhost:8080/fields"
 
-export const listFields = () => axios.get(REST_API_GET_URL);
+export const listFields = () => axios.get(REST_API_URL);
 
-export const createField = (field) => axios.post(REST_API_POST_URL, field);
+export const createField = (field) => axios.post(REST_API_URL + "/create", field);
+
+export const updateField = (updatedField, id) => axios.put(REST_API_URL + "/update/" + id, updatedField) 
+
+export const deleteField = (id) => axios.delete(REST_API_URL + "/delete/" + id)
