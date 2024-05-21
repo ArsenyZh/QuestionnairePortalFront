1e5ff06c997cddd990d93b8faccca62c01d6e7e3
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-05-21 19:27:36 +0300

    add cookies to request

--- src/services/FieldService.js
+++ src/services/FieldService.js
@@ -2,10 +2,18 @@ import axios from 'axios'
 
 const REST_API_URL = "http://localhost:8080/fields"
 
-export const listFields = () => axios.get(REST_API_URL);
+export const listFields = () => axios.get(REST_API_URL, {
+    withCredentials: true
+  })
 
-export const createField = (field) => axios.post(REST_API_URL + "/create", field);
+export const createField = (field) => axios.post(REST_API_URL + "/create", field, {
+  withCredentials: true
+})
 
-export const updateField = (updatedField, id) => axios.put(REST_API_URL + "/update/" + id, updatedField) 
+export const updateField = (updatedField, id) => axios.put(REST_API_URL + "/update/" + id, updatedField, {
+  withCredentials: true
+})
 
-export const deleteField = (id) => axios.delete(REST_API_URL + "/delete/" + id)
+export const deleteField = (id) => axios.delete(REST_API_URL + "/delete/" + id, {
+  withCredentials: true
+})
