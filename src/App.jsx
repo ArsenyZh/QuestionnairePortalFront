105117d3805a7ced810f2855d05eb26e72859aba
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-05-21 19:33:16 +0300

    add response list, questionnaire form and thanks pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import EditProfile from './components/EditProfile'
 import ChangePassword from './components/ChangePassword'
 import LoginForm from './components/LogInForm'
 import SignUpForm from './components/SignUpForm'
+import ResponsesList from './components/ResponsesList'
+import QuestionnaireForm from './components/QuestionnaireForm'
+import Thanks from './components/Thanks'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
 
@@ -18,9 +21,12 @@ export default function App() {
             <Route path='/' element = {<LoginForm />}></Route>
             <Route path='/sign-up' element = {<SignUpForm />}></Route>
             <Route path='/fields' element = {<ListFields />}></Route>
+            <Route path='/responses' element = {<ResponsesList />}></Route>
             <Route path='/add-field' element = {<AddField />}></Route>
             <Route path='/edit-profile' element = {<EditProfile />}></Route>
             <Route path='/change-password' element = {<ChangePassword />}></Route>
+            <Route path='/domainname.com/questionnaires' element = {<QuestionnaireForm />}></Route>
+            <Route path='/domainname.com/questionnaires/successes' element = {<Thanks />}></Route>
           </Routes>
       </BrowserRouter>
     </>
